05aab5340f84edb57c6132f5f8a543117b0baf0b
shewu <shewu19@gmail.com>
2010-05-01 19:46:35 -0400

    first commit

---
+++ gogobot.py
@@ -0,0 +1,29 @@
+class GoGoBot:
+	class Go:
+		def __init__(self):
+			pass
+	
+		def move():
+			# move forward one unit at default speed
+			pass
+	
+		def move(dir):
+			# move in <dir> one unit at default speed
+			pass
+	
+		def move(dir, speed):
+			# move in <dir> one unit at <speed> speed
+			pass
+	
+		def move(dir, dist):
+			# move in <dir> <dist> at default speed
+			pass
+	
+		def move(dir, speed, dist):
+			# move in <dir> <dist> at <speed> speed
+			pass
+
+	class Blink:
+		def __init__(self):
+			pass
+
